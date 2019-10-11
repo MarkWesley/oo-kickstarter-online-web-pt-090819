b3311996c2ac9065b2a5748795e01b749287bc7e
Mark Stone <markwstone85@gmail.com>
2019-10-11 00:31:08 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,8 +2,11 @@ class Backer
   attr_reader :name, :backed_projects
 
   def initialize(name, backed_projects = [])
-    @name = name 
+    @name = name
     @backed_projects = backed_projects
   end
 
+  def back_project(project)
+    @backed_projects << project 
+  end
 end
